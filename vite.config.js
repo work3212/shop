834e776ca26edbd0f95c1ddd5848782dd7ad5516
work3212 <work3212@mail.ru>
2023-10-12 15:11:50 +0000

    Init

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost',
+        }
+    },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css','resources/sass/main.sass', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
